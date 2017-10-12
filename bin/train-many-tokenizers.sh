8415bc77f9e5f49d2f68c44c0436a1217e9aac68
Erwan Moreau <erwan.more@gmail.com>
2017-10-12 17:37:39 +0100

    script train-multiple-tokenizers (replicate main experiment, with options)

--- bin/train-many-tokenizers.sh
+++ bin/train-many-tokenizers.sh
@@ -4,38 +4,73 @@
 
 progName=$(basename "$BASH_SOURCE")
 
-iobOpt=""
+
+trainFilePattern="*train*.conllu"
+testFilePattern="*dev*.conllu"
+
+iso639File="iso639-codes.txt"
+
+testing="yep"
+elman=""
+force=""
+iso639mapping=""
+elephantModelDirName="elephant.model"
 
 function usage {
   echo
-  echo "Usage: $progName [options] <UD directory> <output directory>"
+  echo "Usage: $progName [options] <input directory> <pattern file> <output directory>"
   echo
-  echo "  Reads a list of pattern files from STDIN, applies this pattern to all"
-  echo "  the directories found in <UD directory>, both with and without Elman model."
-  echo "  Corpora are read from <UD directory>/<dataset>/*conllu, using the 'train'"
-  echo "  version for training and the 'dev' version for testing."
+  echo "  Trains multiple tokenizers using pre-tokenized data in .conllu format. This"
+  echo "  script is intended to be used with the Universal Dependencies 2.0 corpus."
+  echo
+  echo "  It is assumed that <input directory> contains one directory by dataset, and"
+  echo "  that every such directory contains a file which matches '$trainFilePattern',"
+  echo "  as well as a file which matches '$testFilePattern'; the former is used for"
+  echo "  training an Elephant model, then the model is applied to the latter, used as"
+  echo "  test data. Additionally, a baseline tokenizer is applied to the test data"
+  echo "  and evaluation is performed for both the trained and the baseline tokenizer."
   echo
   echo "  Options:"
   echo "    -h this help"
-  echo
+  echo "    -t training only; by default the tokenizer model is also applied to the"
+  echo "       test dataset."
+  echo "    -p <train file pattern> pattern to use for finding the train file;"
+  echo "       default: '$trainFilePattern'"
+  echo "    -P <test file pattern> pattern to use for finding the test file;"
+  echo "       default: '$testFilePattern'"
+  echo "    -e use Elman language models (usually performs better but longer training)"
+  echo "    -f force overwriting any previously existing tokenizers in"
+  echo "       <output directory>; by default existing models are not recomputed."
+  echo "    -l generate language codes mapping file; this assumes that the input .conllu"
+  echo "       filenames start with the ISO639 language codes (intended for UD2 corpus)."
+  echo "    -n <elephant model name> name to use for the elephant model directory."
+  echo "       Default: '$elephantModelDirName'."
+  echo 
 }
 
 
 
 
 OPTIND=1
-while getopts 'h' option ; do 
+while getopts 'htp:P:efln:' option ; do 
     case $option in
 	"h" ) usage
  	      exit 0;;
+	"t" ) testing="";;
+	"p" ) trainFilePattern="$OPTARG";;
+	"P" ) testFilePattern="$OPTARG";;
+	"e" ) elman="yep";;
+	"f" ) force="yep";;
+	"l" ) iso639mapping="yep";;
+	"n" ) elephantModelDirName="$OPTARG";;
  	"?" ) 
 	    echo "Error, unknow option." 1>&2
             printHelp=1;;
     esac
 done
 shift $(($OPTIND - 1))
-if [ $# -ne 2 ]; then
-    echo "Error: expecting 2 args." 1>&2
+if [ $# -ne 3 ]; then
+    echo "Error: expecting 3 args." 1>&2
     printHelp=1
 fi
 
@@ -44,53 +79,73 @@ if [ ! -z "$printHelp" ]; then
     exit 1
 fi
 inputDir="$1"
-outputDir="$2"
+patternFile="$2"
+outputDir="$3"
 
-rm -rf $outputDir
+if [ ! -z "$force" ]; then
+    rm -rf $outputDir
+fi
 [ -d "$outputDir" ] || mkdir "$outputDir"
 
-while read patternFile; do
-    p=$(basename "$patternFile")
-    for dataDir in "$inputDir"/*; do
-	echo "PATTERN=$p ; DATA=$dataDir"
-	if [ -d "$dataDir" ]; then
-	    data=$(basename "$dataDir")
-	    trainFile=$(ls $dataDir/*train*.conllu)
-	    ignore=""
-	    if [ -z "$trainFile" ]; then
-		ignore="train"
+#p=$(basename "$patternFile")
+for dataDir in "$inputDir"/*; do
+    if [ -d "$dataDir" ]; then
+	data=$(basename "$dataDir")
+	echo "### Processing '$data'..." 1>&2
+	lsPat="$dataDir/$trainFilePattern"
+	trainFile=$(ls $lsPat 2>/dev/null | head -n 1)
+	if [ -z "$trainFile" ]; then
+	    echo "Warning: no file matches '$lsPat' for training, ignoring dataset '$data'." 1>&2
+	else
+	    testFile=""
+	    if [ ! -z "$testing" ]; then
+		lsPat="$dataDir/$testFilePattern"
+		testFile=$(ls $lsPat 2>/dev/null | head -n 1)
+		if [ -z "$testFile" ]; then
+		    echo "Warning: no file matches '$lsPat' for testing, no testing for dataset '$data'." 1>&2
+		fi
 	    fi
-	    testFile=$(ls $dataDir/*dev*.conllu)
-	    if [ -z "$testFile" ]; then
-		ignore="dev"
-	    fi
-	    if [ -z "$ignore" ]; then
+	    if [ ! -z "$trainFile" ]; then 
 		workDir="$outputDir/$data"
+		opts=""
 		[ -d "$workDir" ] || mkdir "$workDir"
-		if [ ! -s "$workDir/elman.model" ]; then
+		processWithElman=""
+		if [ ! -z "$elman" ] &&  [ ! -s "$workDir/elman.model" ]; then # Training Elman LM
 		    train-lm-from-UD-corpus.sh "$trainFile" "$workDir/elman.model"
+		    opts="$opts -e \"$workDir/elman.model\""
+		    if [ ! -s "$workDir/$elephantModelDirName/elman" ]; then
+			processWithElman="yep"
+		    fi
 		fi
-		if [ ! -s "$workDir/baseline.eval" ]; then # apply generic tokenizer to test data
-		    # 1. get text file from test UD conllu file
-		    untokenize.pl -f UD -C 1 "$testFile" >"$workDir/baseline.txt"
-		    generic-tokenizer.pl -B T -i "$workDir/baseline.txt" >"$workDir/baseline.out"
-		    # 2. get IOB gold output
-		    untokenize.pl -B T -i -f UD -C 1 "$testFile" >"$workDir/gold.out"
-		    # 3. eval
-		    evaluate.pl "$workDir/baseline.out:2" "$workDir/gold.out:2" >"$workDir/baseline.eval"
-		    cat "$workDir/baseline.eval"
+		if [ ! -z "$processWithElman" ] || [ ! -s "$workDir/$elephantModelDirName/wapiti" ]; then # Training main Wapiti model
+		    command="train-tokenizer-from-UD-corpus.sh $opts \"$trainFile\" \"$patternFile\" \"$workDir/$elephantModelDirName\""
+		    eval "$command"
+		    if [ ! -s "$workDir/$elephantModelDirName/wapiti" ]; then
+			echo "An error occured during training. Command was: '$command'" 1>&2
+			echo "Skipping dataset '$data'" 1>&2
+			testFile="" # skip testing
+		    fi
+		fi
+		if [ ! -z "$testFile" ]; then # testing
+		    # get IOB gold output
+		    untokenize.pl -i -f UD -C 1 -B T "$testFile" >"$workDir/gold.iob"
+		    # remark: evaluation is also done by tokenize.sh
+		    tokenize.sh -c -I -i "$testFile" -o "$workDir/test.iob"  "$workDir/$elephantModelDirName"
 
-		    
+		    # the following 3 steps are for baseline tokenizer only:
+		    # 1. get text file from test UD conllu file
+		    untokenize.pl -f UD -C 1 -B T "$testFile" >"$workDir/baseline.txt"
+		    # 2. tokenize it with baseline tokenizer
+		    generic-tokenizer.pl -B T -i "$workDir/baseline.txt" >"$workDir/baseline.iob"
+		    rm -f "$workDir/baseline.txt"
+		    # 3. evaluate baseline
+		    evaluate.pl "$workDir/baseline.iob:2" "$workDir/gold.iob:2" >"$workDir/baseline.eval"
 		fi
-		workDir2="$workDir/$p"
-		[ -d "$workDir2" ] || mkdir "$workDir2"
-		train-tokenizer-from-UD-corpus.sh "$trainFile" "$patternFile" "$workDir2/crf.model"
-		tokenize.sh -c -I -i "$testFile" -o "$workDir2/crf.output"  "$workDir2/crf.model" 
-		train-tokenizer-from-UD-corpus.sh -e "$workDir/elman.model" "$trainFile" "$patternFile" "$workDir2/elman.model"
-		tokenize.sh -c -I -i "$testFile" -o  "$workDir2/elman.output" "$workDir2/elman.model" 
-	    else
-		echo "Warning: no $ignore file in $dataDir, ignoring" 1>&2
 	    fi
 	fi
-    done
+    fi
 done
+if [ ! -z "$iso639mapping" ]; then
+    echo "### Generating ISO639 codes mapping..." 1>&2
+    generate-iso639-codes-file.sh "$inputDir" >"$outputDir/$iso639File"
+fi
