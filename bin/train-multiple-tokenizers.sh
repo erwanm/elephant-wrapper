b51d29139f6ff7235602deeb39bda15f5df38f68
Erwan Moreau <erwan.more@gmail.com>
2017-10-12 20:06:46 +0100

    added split dev file if no train file in train multi tokenizer

--- bin/train-multiple-tokenizers.sh
+++ bin/train-multiple-tokenizers.sh
@@ -15,6 +15,7 @@ elman=""
 force=""
 iso639mapping=""
 elephantModelDirName="elephant.model"
+splitDevFile=""
 
 function usage {
   echo
@@ -45,6 +46,10 @@ function usage {
   echo "       filenames start with the ISO639 language codes (intended for UD2 corpus)."
   echo "    -n <elephant model name> name to use for the elephant model directory."
   echo "       Default: '$elephantModelDirName'."
+  echo "    -s <training set proportion> Split test file into training and test set if "
+  echo "       no training file is found (this is a workaround for the two languages in"
+  echo "       UD2 for which only a dev file is supplied: UD_Kazakh and UD_Uyghur)."
+  echo "       Example: -s 0.8 means 80% train set, 20% test set."
   echo 
 }
 
@@ -52,7 +57,7 @@ function usage {
 
 
 OPTIND=1
-while getopts 'htp:P:efln:' option ; do 
+while getopts 'htp:P:efln:s:' option ; do 
     case $option in
 	"h" ) usage
  	      exit 0;;
@@ -63,6 +68,7 @@ while getopts 'htp:P:efln:' option ; do
 	"f" ) force="yep";;
 	"l" ) iso639mapping="yep";;
 	"n" ) elephantModelDirName="$OPTARG";;
+	"s" ) splitDevFile="$OPTARG";;
  	"?" ) 
 	    echo "Error, unknow option." 1>&2
             printHelp=1;;
@@ -87,25 +93,34 @@ if [ ! -z "$force" ]; then
 fi
 [ -d "$outputDir" ] || mkdir "$outputDir"
 
-#p=$(basename "$patternFile")
 for dataDir in "$inputDir"/*; do
     if [ -d "$dataDir" ]; then
+	cleanupFiles=""
 	data=$(basename "$dataDir")
 	echo "### Processing '$data'..." 1>&2
-	lsPat="$dataDir/$trainFilePattern"
-	trainFile=$(ls $lsPat 2>/dev/null | head -n 1)
-	if [ -z "$trainFile" ]; then
-	    echo "Warning: no file matches '$lsPat' for training, ignoring dataset '$data'." 1>&2
+	lsPatTrain="$dataDir/$trainFilePattern"
+	trainFile=$(ls $lsPatTrain 2>/dev/null | head -n 1)
+	lsPatTest="$dataDir/$testFilePattern"
+	testFile=$(ls $lsPatTest 2>/dev/null | head -n 1)
+	if [ -z "$trainFile" ] && [ -z "$testFile" ]; then
+	    echo "Warning: no file matches '$lsPatTrain' neither '$lsPatTest', ignoring dataset '$data'." 1>&2
 	else
-	    testFile=""
-	    if [ ! -z "$testing" ]; then
-		lsPat="$dataDir/$testFilePattern"
-		testFile=$(ls $lsPat 2>/dev/null | head -n 1)
-		if [ -z "$testFile" ]; then
-		    echo "Warning: no file matches '$lsPat' for testing, no testing for dataset '$data'." 1>&2
+	    # from here we have either a train set or a test set
+	    if [ -z "$trainFile" ] && [ -z "$splitDevFile" ]; then
+		echo "Warning: no file matches '$lsPatTrain' for training and no option -s, ignoring dataset '$data'." 1>&2
+	    else
+		# from here we have a train set or a test + split option (or both)
+		if [ -z "$trainFile" ]; then # no train set: we split the test set
+		    echo "Warning: no file matches '$lsPatTrain' in '$data'; splitting test file '$testFile'." 1>&2
+		    prefix=$(mktemp --tmpdir "$progName.split-dev-file.XXXXXXXXXX")
+		    split-conllu-sentences.pl -b "$prefix." -p "$splitDevFile" "$testFile"
+		    trainFile="$prefix.1"
+		    testFile="$prefix.2"
+		    cleanupFiles="$cleanupFiles $prefix.1 $prefix.2"
 		fi
-	    fi
-	    if [ ! -z "$trainFile" ]; then 
+		# now we have a train set, but it's still possible we don't have a test set; we're doing the training anyway
+
+		# TRAINING
 		workDir="$outputDir/$data"
 		opts=""
 		[ -d "$workDir" ] || mkdir "$workDir"
@@ -126,22 +141,31 @@ for dataDir in "$inputDir"/*; do
 			testFile="" # skip testing
 		    fi
 		fi
-		if [ ! -z "$testFile" ]; then # testing
-		    # get IOB gold output
-		    untokenize.pl -i -f UD -C 1 -B T "$testFile" >"$workDir/gold.iob"
-		    # remark: evaluation is also done by tokenize.sh
-		    tokenize.sh -c -I -i "$testFile" -o "$workDir/test.iob"  "$workDir/$elephantModelDirName"
 
-		    # the following 3 steps are for baseline tokenizer only:
-		    # 1. get text file from test UD conllu file
-		    untokenize.pl -f UD -C 1 -B T "$testFile" >"$workDir/baseline.txt"
-		    # 2. tokenize it with baseline tokenizer
-		    generic-tokenizer.pl -B T -i "$workDir/baseline.txt" >"$workDir/baseline.iob"
-		    rm -f "$workDir/baseline.txt"
-		    # 3. evaluate baseline
-		    evaluate.pl "$workDir/baseline.iob:2" "$workDir/gold.iob:2" >"$workDir/baseline.eval"
+		# TESTING
+		if [ ! -z "$testing" ]; then
+		    if [ -z "$testFile" ]; then
+			echo "Warning: no file matches '$lsPatTest' for '$data', skipping testing." 1>&2
+		    else
+			# get IOB gold output
+			untokenize.pl -i -f UD -C 1 -B T "$testFile" >"$workDir/gold.iob"
+			# remark: evaluation is also done by tokenize.sh
+			tokenize.sh -c -I -i "$testFile" -o "$workDir/test.iob"  "$workDir/$elephantModelDirName"
+
+			# the following 3 steps are for baseline tokenizer only:
+			# 1. get text file from test UD conllu file
+			untokenize.pl -f UD -C 1 -B T "$testFile" >"$workDir/baseline.txt"
+			# 2. tokenize it with baseline tokenizer
+			generic-tokenizer.pl -B T -i "$workDir/baseline.txt" >"$workDir/baseline.iob"
+			rm -f "$workDir/baseline.txt"
+			# 3. evaluate baseline
+			evaluate.pl "$workDir/baseline.iob:2" "$workDir/gold.iob:2" >"$workDir/baseline.eval"
+		    fi
 		fi
 	    fi
+	fi
+	if [ ! -z "$cleanupFiles" ]; then
+	    rm -f $cleanupFiles
 	fi
     fi
 done
