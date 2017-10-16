a40fe70189b29ef8023652db876c5dc53198639d
Erwan Moreau <erwan.more@gmail.com>
2017-10-16 21:11:07 +0100

    fixed test

--- tests/01-tokenize.sh
+++ tests/01-tokenize.sh
@@ -3,7 +3,9 @@
 text="Hello my old friend, why you didn't call?"
 echo "Tokenizing en: \"$text\""
 echo "$text" | tokenize.sh en
+echo
 
 text="Salut les amis! On s'amuse bien, n'est-ce pas ?"
 echo "Tokenizing fr: \"$text\""
 echo "$text" | tokenize.sh fr
+echo
