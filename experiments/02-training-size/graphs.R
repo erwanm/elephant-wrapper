6dff00af79dabc48aebf18bbf168aec644b6ca60
Erwan Moreau <erwan.more@gmail.com>
2018-02-10 21:01:29 +0000

    graphs vary size

--- experiments/02-training-size/graphs.R
+++ experiments/02-training-size/graphs.R
@@ -1,18 +1,26 @@
 library(ggplot2)
+library(plyr)
 
 loadData <- function(f='experiments/02-training-size/results/all-results-100-s12200.tsv') {
   read.table(f,header=TRUE)
 }
 
-boxplots <- function(d,nb=0,ymin=0) {
+boxplots <- function(d,nb=0,ymin=0,withnotch=FALSE) {
   if (nb>0) {
     modu<-round(max(d$no)/nb,0)
     print(modu)
     d<- d[d$no %% modu==0,]
   }
-  g<-ggplot(d,aes(as.factor(nbSentences),perf))+geom_boxplot()
+  g<-ggplot(d,aes(as.factor(nbSentences),perf))+geom_boxplot(notch=withnotch)
   if (ymin!=0) {
-    g<-g+ylim(c(ymin,1))
+    g<-g+coord_cartesian(ylim=c(ymin,1)) 
   }
   g
+}
+
+
+meanByNbSent <- function(d) {
+  ddply(d,'nbSentences',function(df) {
+    data.frame(meanperf=mean(df$perf),medianperf=median(df$perf))
+  })
 }
