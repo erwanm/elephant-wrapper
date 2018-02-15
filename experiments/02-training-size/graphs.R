99ea2e90552b0e4da085e3a441ecd910bca041e6
Erwan Moreau <erwan.more@gmail.com>
2018-02-15 17:22:34 +0000

    graphs expe training size

--- experiments/02-training-size/graphs.R
+++ experiments/02-training-size/graphs.R
@@ -1,17 +1,17 @@
 library(ggplot2)
 library(plyr)
 
-loadData <- function(f='experiments/02-training-size/results/all-results-100-s12200.tsv') {
+loadData <- function(f='experiments/02-training-size/results-10-8900/all-results.tsv') {
   read.table(f,header=TRUE)
 }
 
-boxplots <- function(d,nb=0,ymin=0,withnotch=FALSE) {
+boxplots <- function(d,nb=0,ymin=0,withnotch=FALSE,fontsize=26) {
   if (nb>0) {
     modu<-round(max(d$no)/nb,0)
     print(modu)
     d<- d[d$no %% modu==0,]
   }
-  g<-ggplot(d,aes(as.factor(nbSentences),perf))+geom_boxplot(notch=withnotch)
+  g<-ggplot(d,aes(as.factor(nbSentences),perf))+geom_boxplot(notch=withnotch)+ scale_y_continuous(labels = scales::percent)+xlab('Number of sentences') + theme_grey(base_size = fontsize)+theme(axis.title.y=element_blank())
   if (ymin!=0) {
     g<-g+coord_cartesian(ylim=c(ymin,1)) 
   }
@@ -21,6 +21,6 @@ boxplots <- function(d,nb=0,ymin=0,withnotch=FALSE) {
 
 meanByNbSent <- function(d) {
   ddply(d,'nbSentences',function(df) {
-    data.frame(meanperf=mean(df$perf),medianperf=median(df$perf))
+    data.frame(meanperf=mean(df$perf),medianperf=median(df$perf),sd(df$perf))
   })
 }
